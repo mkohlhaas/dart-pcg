182ef404a95bc19828b3ada46d929e9e9802e30f
Hans-J. Schmid <knock@myopendoor.de>
2023-11-06 15:21:17 +0100

    initializes repository.

---
+++ example/dart_pcg_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_pcg/dart_pcg.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/dart_pcg_test.dart
@@ -0,0 +1,16 @@
+import 'package:dart_pcg/dart_pcg.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
